c2d62056bc320a46c7e1578c62c6b9ea26482cf5
1982Rajiv <146075574+1982Rajiv@users.noreply.github.com>
2023-09-29 17:46:32 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+export default class Index extends NextPage {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my Next.js project!</h1>
+      </div>
+    );
+  }
+}
